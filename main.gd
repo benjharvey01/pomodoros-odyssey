158126e1af17a0b9bcaacbe528b665e4242e08e0
Ben Harvey <benjharvey01@gmail.com>
2024-02-16 06:06:16 +1100

    Fixing timer and adding pause button

--- main.gd
+++ main.gd
@@ -1,14 +1,21 @@
 extends Node2D
-
-var WorkingSessionTimeLeft = ceil($WorkingSessionTimer.get_time_left())
-
-func _process(delta):
-	$WorkingSessionTimerText.set_text(str(WorkingSessionTimeLeft))
-	pass
-
-func _on_working_session_start_button_pressed():
-	print_debug("working session has started!")
-	$WorkingSessionTimer.start()
 	
+func _process(delta: float):
+	$WorkingSessionTimerText.text = "%s" % ceil($WorkingSessionTimer.time_left)
+
 func _on_working_session_timer_timeout():
-	print_debug("working session has ended!")
+	print_debug("working session complete!")
+	
+func _on_working_session_start_button_pressed():
+	print_debug("working session started!")
+	$WorkingSessionTimer.start()
+
+func _on_working_session_pause_button_pressed():
+	pause_timer($WorkingSessionTimer)
+	
+# Pauses / unpauses a given timer.
+func pause_timer(Timer):
+	Timer.paused = !Timer.paused
+
+
+
